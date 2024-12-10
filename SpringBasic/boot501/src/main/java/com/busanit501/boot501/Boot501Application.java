f4d5d5a6e0810df402a187dcda648f33b6fb3ed6
Duhyeon Kang <rkdengus1234@naver.com>
2024-12-10 15:59:43 +0900

    241210 5,6교시

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/Boot501Application.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/Boot501Application.java
@@ -2,8 +2,10 @@ package com.busanit501.boot501;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 베이스 엔티티를 이용할 수 있게, 전역에 설정하는 코드
 public class Boot501Application {
 
 	public static void main(String[] args) {
